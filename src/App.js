2c14c4c42d1b079ffef86c49713f9d886d909d7d
predeyo <predeyo@tutanota.com>
2020-04-28 17:17:24 +0300

    Created dropdown component, added global styles for page element box shadow, added dark theme class to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 
+import Header from './components/Header/Header';
+import Dropdown from './components/Dropdown/Dropdown.js';
+
 import './App.scss';
-import Header from "./components/Header/Header";
 
 const App = () => {
-  // Set theme to light by default
-  const [theme, setTheme] = useState('light');
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-  return (
-    <div className="App">
-      <Header theme={theme} toggleTheme={toggleTheme} />
-      <button onClick={toggleTheme}>Toggle Theme</button>
-      <h1>Current theme: {theme}</h1>
-    </div>
-  );
+    // Set theme to light by default
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+    const [region, setRegion] = useState('');
+    return (
+        <div className={`App ${theme}`}>
+            <Header theme={theme} toggleTheme={toggleTheme} />
+            {/* Temporary example - DELETE when country list is created */}
+            <Dropdown
+                buttonText="Filter by Region"
+                list={['Africa', 'America', 'Asia', 'Europe', 'Oceania']}
+                selectedItem={region}
+                onSelect={setRegion}
+                theme={theme}
+            />
+            <h1>{region}</h1>
+            {/* ----------------- */}
+        </div>
+    );
 };
 
 export default App;
