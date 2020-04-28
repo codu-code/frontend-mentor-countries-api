d49fbdaca0d6483bccdca64d1a30f301d4c8ee63
predeyo <59302687+predeyo@users.noreply.github.com>
2020-04-28 21:07:34 +0300

    Created dropdown component, added global styles for page element box shadow, added dark theme class to App (#5)

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import styles from './Dropdown.module.scss';
+
+/*
+  Dropdown:
+  buttonText - text displayed on dropdown button
+  list - list of dropdown options
+  onSelect - function fired when item is selected
+  selectedItem - currently selected item, used for styling background of the item
+  theme - 'dark'/'light', defaults to light
+*/
+const Dropdown = ({ buttonText, list, onSelect, selectedItem, theme }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleSelect = item => {
+        toggleDropdown();
+        onSelect(item);
+    };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <div
+            className={`
+              ${styles.Dropdown}
+              ${theme === 'dark' ? styles.dark : ''}
+            `}
+        >
+            <div className={styles.clickArea} onClick={toggleDropdown}>
+                <span>{buttonText}</span>
+                {isOpen ? (
+                    <i className="fas fa-angle-down" />
+                ) : (
+                    <i className="fas fa-angle-up" />
+                )}
+            </div>
+            <ul className={isOpen ? styles.open : ''}>
+                {list.map(item => (
+                    <li
+                        key={item}
+                        className={item === selectedItem ? styles.selected : ''}
+                        onClick={() => handleSelect(item)}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Dropdown;
